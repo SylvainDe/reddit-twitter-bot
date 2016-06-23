8ecd1d877fa8f6b75321ef0e9a7de86955ca075f
Sylvain <sylvain.desodt+github@gmail.com>
2016-06-23 22:05:47 +0200

    Simplify the objects returned by get_reddit_posts
    Give the function a more meaningful name
    Make the function return a simple list of posts
    instead of 2 objects.

--- reddit_twitter_bot.py
+++ reddit_twitter_bot.py
@@ -61,38 +61,33 @@ def setup_connection_reddit(subreddit):
     return reddit_api.get_subreddit(subreddit)
 
 
-def tweet_creator(subreddit_info):
-    ''' Looks up posts from reddit and shortens the URLs to them. '''
-    post_dict = {}
-    post_ids = []
-
+def get_reddit_posts(subreddit_info):
+    ''' Looks up posts from reddit and returns them. '''
     print('[bot] Getting posts from reddit')
 
+    posts = []
     # You can use the following "get" functions to get posts from reddit:
     #   - get_top(): gets the most-upvoted posts (ignoring post age)
     #   - get_hot(): gets the most-upvoted posts (taking post age into account)
     #   - get_new(): gets the newest posts
     #
     # "limit" tells the API the maximum number of posts to look up
-
     for submission in subreddit_info.get_hot(limit=5):
         if not already_tweeted(submission.id):
-            # This stores a link to the reddit post itself
-            # If you want to link to what the post is linking to instead, use
-            # "submission.url" instead of "submission.permanlink"
-            post_dict[submission.title] = {}
-            post = post_dict[submission.title]
-            post['link'] = submission.permalink
-
-            # Store the url the post points to (if any)
-            # If it's an imgur URL, it will later be downloaded and uploaded alongside the tweet
-            post['img_path'] = get_image(submission.url)
-
-            post_ids.append(submission.id)
+            posts.append({
+                'id': submission.id,
+                'title': submission.title,
+                # This stores a link to the reddit post itself
+                # If you want to link to what the post is linking to instead, use
+                # "submission.url" instead of "submission.permanlink"
+                'link': submission.permalink,
+                # Store the url the post points to (if any)
+                # If it's an imgur URL, it will later be downloaded and uploaded alongside the tweet
+                'url': submission.url,
+            })
         else:
             print('[bot] Already tweeted: {}'.format(str(submission)))
-
-    return post_dict, post_ids
+    return posts
 
 
 def already_tweeted(post_id):
@@ -138,19 +133,21 @@ def get_image(img_url):
     return ''
 
 
-def tweeter(post_dict, post_ids):
+def tweeter(posts):
     ''' Tweets all of the selected reddit posts. '''
     auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
     auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
     api = tweepy.API(auth)
 
-    for post, post_id in zip(post_dict, post_ids):
-        img_path = post_dict[post]['img_path']
-        extra_text = ' ' + post_dict[post]['link'] + TWEET_SUFFIX
+    for post in posts:
+        post_id = post['id']
+        img_path = get_image(post['url'])
+        post_title = post['title']
+        extra_text = ' ' + post['link'] + TWEET_SUFFIX
         extra_text_len = 1 + T_CO_LINKS_LEN + len(TWEET_SUFFIX)
         if img_path:  # Image counts as a link
             extra_text_len += T_CO_LINKS_LEN
-        post_text = strip_title(post, TWEET_MAX_LEN - extra_text_len) + extra_text
+        post_text = strip_title(post_title, TWEET_MAX_LEN - extra_text_len) + extra_text
         print('[bot] Posting this link on Twitter')
         print(post_text)
         if img_path:
@@ -178,8 +175,8 @@ def main():
         os.makedirs(IMAGE_DIR)
 
     subreddit = setup_connection_reddit(SUBREDDIT_TO_MONITOR)
-    post_dict, post_ids = tweet_creator(subreddit)
-    tweeter(post_dict, post_ids)
+    posts = get_reddit_posts(subreddit)
+    tweeter(posts)
 
     # Clean out the image cache
     for filename in glob(IMAGE_DIR + '/*'):
